58ceb0ab496859815c98f3124ee192713d090cbd
FrancoisMaki <francoismakijess@gmail.com>
2025-07-01 11:09:07 +0200

    resolve some bugs

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 from components.controller.country_controller import pais_bp
 
-app = Flask(__name__, static_folder='public')
+app = Flask(__name__, static_folder='static')
 
 app.register_blueprint(pais_bp)
 
-# Ruta para servir el HTML
+# Ruta principal que carga el HTML desde /templates
 @app.route('/')
 def index():
-    return send_from_directory('public/html', 'countries.html')
-
-# Opcional: si quieres servir js directamente, no es necesario si static_folder está bien
-@app.route('/js/<path:filename>')
-def serve_js(filename):
-    return send_from_directory('public/js', filename)
+    return render_template('countries.html')  # Busca en templates/countries.html
 
 if __name__ == '__main__':
     app.run(debug=True)
